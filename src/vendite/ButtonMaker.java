d0aec26607dff906180f3801ea79c89c9473f632
Roberto Cardaccia <robcardaccia22@gmail.com>
2016-04-30 11:45:22 +0200

    non funzionante da finire

--- src/vendite/ButtonMaker.java
+++ src/vendite/ButtonMaker.java
@@ -1,10 +1,22 @@
 package vendite;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+//classe per creare bottoni da file
 public class ButtonMaker {
     
     
+    static void make() {
+        File f = new File("brands");
+        ArrayList<String> names = new ArrayList<>(Arrays.asList(f.list())); 
     
-    
+        for(String elem:names) {
+            
+        }
+    }
     
     
     
