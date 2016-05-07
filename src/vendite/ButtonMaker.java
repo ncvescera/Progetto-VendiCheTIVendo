a95c55723f76a0552f3d656ba917417825b95ed7
Roberto Cardaccia <robcardaccia22@gmail.com>
2016-05-07 10:48:57 +0200

    completo

--- src/vendite/ButtonMaker.java
+++ src/vendite/ButtonMaker.java
@@ -3,19 +3,28 @@ package vendite;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javax.swing.JButton;
 
 
 //classe per creare bottoni da file
 public class ButtonMaker {
     
     
-    static void make() {
+    public static String[] make() {
         File f = new File("brands");
         ArrayList<String> names = new ArrayList<>(Arrays.asList(f.list())); 
-    
-        for(String elem:names) {
-            
+        
+        String[] da_ritornare = new String[names.size()];
+        names.toArray(da_ritornare);
+        
+        ArrayList<JButton> btnTemp = new ArrayList();
+        for (int i = 0; i < da_ritornare.length; i++) {
+           btnTemp.add(new JButton(da_ritornare[i]));
         }
+        
+        
+        
+        return da_ritornare;
     }
     
     
